d94220e49815ce0710686cc7055690f7c2f3c456
Javier Carrizo <javiercarrizo15@gmail.com>
2018-06-12 00:10:20 -0400

    publish and social core

--- SocialNeet.sol
+++ SocialNeet.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.4.24;
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
-
- 
 library SafeMath {
 
   /**
@@ -52,18 +50,15 @@ library SafeMath {
   }
 }
 
-
 contract Publish {
-
+    using SafeMath for uint256;
 
     struct Content {
         address content_owner;
         string ipfs_content;
     }
 
-
-    uint256 shares;
-    
+    uint256 public shares;
     struct Influencer{
        bool top5;
        bool shared;
@@ -72,109 +67,185 @@ contract Publish {
 
     mapping(address => Influencer ) public Influencers;
 
-    uint public ammount_raised;
-    uint public ammount_;
+    // 
+    address socialnet_adr;
+    uint256 public gas_ammount_to_stop;
+    uint256 public gas_ammount_to_end;
+    bool public finished;
+    uint256[2]  public Pammount_;
+    uint256 public ammount_;
     address public owner;
     address [] public BNS;
-    mapping(address => Influencer) public Influencers;
+    address [] public BNS_finish;
+    uint256 collected;
+    // mapping(address => Influencer) public Influencers;
     uint minimum_I_value;
+    
+    uint256 public debug_gas;
 
-
-    function publish(Content cont) public payable {
+     constructor (address _address) public payable {
+        owner=msg.sender;
+        ammount_ = msg.value;
+        debug_gas = msg.gas;
         require(msg.value != 0 );
-        require();
+        finished=false;
+        collected=0;
+        gas_ammount_to_stop=0;
+        gas_ammount_to_end=0;
         
     }
 
-
-    function shareEvent(address _address){
-      require(msg.gas >= add(gas_ammount_to_stop,gas_ammount_to_share));
-      require(msg.gas >= add(gas_ammount_to_end,gas_ammount_to_share));
-      require(Influencers[_address].shared != true);
-      Influencers[_address].shared = true;
-      minimum = get_min_inf();
-      ammount_raised = ;
-      if (Influencers[_address].followers > minimum[0] ){
-        BNS[minimum[1]] = _address;
-        Influencers[BNS[minimum[1]]].top = false;
-        Influencers[_address].top5 = true;
-      }
-
+    function shareEvent  (address _address) public{
+      if (msg.gas >= gas_ammount_to_stop){
+          if(msg.gas >= gas_ammount_to_end){
+                    uint256 [2] memory minimum;
+                //   require(msg.gas >= add(gas_ammount_to_stop,gas_ammount_to_share));
+                //   require(msg.gas >= add(gas_ammount_to_end,gas_ammount_to_share));
+                  require(Influencers[_address].shared != true);
+                  Influencers[_address].shared = true;
+                  minimum = get_min_inf();
+                //   ammount_raised = ;
+                
+                
+                // Influencers Followers---> Users Followers.
+                  if (Influencers[_address].followers > minimum[0] ){
+                    BNS[minimum[1]] = _address;
+                    Influencers[BNS[minimum[1]]].top5 = false;
+                    Influencers[_address].top5 = true;
+                    shares += 1;
+                  }
+                  
+          }else {}
+      }else {Finish();}
+     
+    }
+    function Finish() public {
+        
+        finished=true;
+        Pammount_ = EffortPayment();
+        socialnet_adr.transfer(Pammount_[1]);
     }
 
-    function get_min_inf(){
+    function get_min_inf() public returns(uint256[2] memory){
+        // index
       uint min;
-      uint min_addr;
+      uint min_addr_count;
+      uint256 [2] memory minimal;
       if(BNS.length > 1){
-        min = Influencers[BNS[0]];
-        for (uint i=1; i< BNS.length){
+        min = Influencers[BNS[0]].followers;
+        for (uint i=1; i< BNS.length ;i++){
             if (Influencers[BNS[i]].followers < min){
               min = Influencers[BNS[i]].followers;
               min_addr_count = i;
             }
         }
+        minimal[0] = min;
+        minimal[1] = min_addr_count;
       }
-      return min,min_addr_count
+      
+      return minimal;
     }
 
-    function AuthorStopCollect(){
+    // function ownerCollect(address _address){
+    //     require(owner==_address);
+    //     owner.transfer(PAmmount[1]);
+    // }
+
+    function AuthorStopCollect() public {
         require(msg.sender == owner);
+        uint256[2] memory EachAmmount;
         EachAmmount = EffortPayment();
         if(BNS.length >0){
-          for (uint i=1; i< BNS.length){
-              BNS[i].transfer(EachAmmount);
+          for (uint i=1; i< BNS.length;i++){
+              BNS[i].transfer(EachAmmount[0]);
           }
          
         }
-        socialnet_adr.transfer(div(mul(30,msg.value),100));
+        socialnet_adr.transfer(EachAmmount[1]);
         selfdestruct(owner);
     }
 
-        
-    function EffortPayment() { 
-                for (uint i=0 ; i < BNS.length; i++){
-                }
+
+    function EffortPayment() public returns(uint256 [2] memory ){ 
+        // easy and boring proposal  all equals
+                uint256[2] memory AmmountAll;
+                uint256 AmmountSN;
+                uint256 Ammount;
+                AmmountAll[0]=SafeMath.div(SafeMath.div(SafeMath.mul(75,msg.value),100),BNS.length);
+                AmmountAll[1]= msg.value - Ammount;
+                return AmmountAll;
         }
-    }
     
 
-    function UserCollect(address _address) {
-          require( Influencers[_address].top5 == true)
-          _address.transfer(EachAmmount);
+    function UserCollect(address _address) public {
+        require(finished=true);
+        
+          require( Influencers[_address].top5 == true);
+          _address.transfer(Pammount_[0]);
+          collected += collected;
+          if(collected==BNS.length){
+              selfdestruct(owner);
+          }
         
     }
 
-}
 
+}
 
 contract SocialCore {
     
+    
+    address socialnet_adr;
+    uint256 debug_gas_sc;
+    struct Follow {
+        bool is_f;
+    }
+    
     struct User {
-        address adr;
         string ipns_user_profile;
         string ipns_user_post;
-        address [] followers;
+        mapping (address => Follow) Followers;
+        
+    }
+    mapping(address => User ) public Users;  
+    
+    constructor(address _addresso){
+                socialnet_adr = msg.sender;
+                debug_gas_sc = msg.gas;
+        
     }
     
-
-
-    function OpenContentToCollect()
-    {
-
-
+    function CreateUser(address addresu, string ipnsProf, string ipnsPost) public payable{
+        Users[addresu].ipns_user_profile = ipnsProf;
+        Users[addresu].ipns_user_post = ipnsPost;
+        
     }
+
+
+    function AddFollowerFS(address addressu,address addressf) public 
+    {
+        Users[addressu].Followers[addressf].is_f=true;
+    }
+
+
+
+    // function OpenContentToCollect()
+    // {
+
+
+    // }
     
-    function MaintenanceRedistribution()
-    {
+    // function MaintenanceRedistribution()
+    // {
 
 
-    }
+    // }
 
-    function GitUsersDevTeam()
-    {
+    // function GitUsersDevTeam()
+    // {
+    //# QualityCommits
 
 
-    }
+    // }
     
 }
-
