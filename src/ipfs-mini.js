8eaac15b856990bea20ec5e052819a06a0f0837d
Joaquin Ugarte <joaquinu@Joaquins-MacBook-Pro.local>
2018-06-17 01:10:07 -0400

    fix ipfs

--- src/ipfs-mini.js
+++ src/ipfs-mini.js
@@ -1,8 +1,6 @@
-const ipfsAPI = require('ipfs-api');
 const XMLHttpRequest = require('./lib/XMLHttpRequest');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-//module.exports = IPFS;
+module.exports = IPFS;
 
 /**
  * The constructor object
@@ -15,39 +13,6 @@ function IPFS(provider) {
 
   const self = this;
   self.setProvider(provider || {});
-
-uploadBlob(blob) {
-    var reader = new FileReader();
-    var result = new Promise((resolve, reject) => {
-      reader.onload = async () => {
-        var result = await this.node.files.add([Buffer.from(reader.result)]);
-        resolve(result[0].hash);
-      };
-    });
-    reader.readAsArrayBuffer(blob);
-    return result;
-  }
-
-  loadImage(img, hash) {
-    this.node.files.cat(hash).then((stream) => {
-      var buf = [];
-      var blob;
-      stream.on('data', (file) => {
-        var data = Array.prototype.slice.call(file);
-        buf = buf.concat(data);
-      });
-      stream.on('end', (file) => {
-        if (typeof blob !== 'undefined') {
-          window.URL.revokeObjectURL(blob);
-        }
-
-        buf = new Buffer(buf);
-        blob = new Blob([buf], { type: 'image/jpg' });
-        img.src = window.URL.createObjectURL(blob);
-        img.srcset = '';
-      });
-    });
-  }
 
 }
 
