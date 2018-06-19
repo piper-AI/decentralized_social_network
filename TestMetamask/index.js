52e6b758facce69f389da0b74a863856cf70a5cc
Javier Carrizo <javiercarrizo15@gmail.com>
2018-06-19 19:48:56 -0400

    metamask testing

---
+++ TestMetamask/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000
+// const publicPath = path.join(__dirname,'./static');
+
+app.set('view engine','hbs');
+
+// app.use(express.static(publicPath));
+
+app.get('/', function (req, res) {
+  res.render('testJavier.hbs')
+})
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
